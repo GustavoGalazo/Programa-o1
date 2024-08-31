6904f782bf045e8900693a7461a9dff9018446d8
gustavo galazo <gustavo.galazo@etec.sp.gov.br>
2024-08-31 09:31:23 -0300

    1

---
+++ Aula02/01-estrutura-sequencial/Ex02.py
@@ -0,0 +1,9 @@
+"""
+Exercício 02 – Crie um programa que exibe a soma de dois números inteiros.
+Saída esperada: 'A soma de 20 e 25 é 45.'
+"""
+
+num1 = 10
+num2 = 25
+
+# Seu código aqui
---
+++ Aula03/03-formatacao-decimal/Ex01.py
@@ -0,0 +1,23 @@
+"""
+Exercício 08 – Crie um programa que solicita duas notas (n1 e n2)
+e calcule a média aritmética dessas notas.
+Exiba o resultado usando duas casas decimais.
+
+Entrada:
+Digite a primeira nota: 8
+Digite a segunda nota: 7
+
+Saída:
+A média aritmética é: 7.50
+
+"""
+
+n1 = float(input("Digite um valor: "))
+n2 = float(input("Digite o segundo valor: "))
+
+media = (n1 + n2) / 2
+
+print(f"A média aritmética é: {media}")
+
+
+
